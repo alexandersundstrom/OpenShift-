afea6d92539a9619847ce8d6c0b2ff6dbd5e7368
Alexander Sundström <alexander.sundstrom@arbetsformedlingen.se>
2021-01-08 13:46:27 +0100

    Removes param

--- src/main/java/rest/controller/PingController.java
+++ src/main/java/rest/controller/PingController.java
@@ -4,10 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Objects;
 
 
 @RestController
@@ -17,10 +14,7 @@ public class PingController {
     private final Logger log = LoggerFactory.getLogger(PingController.class);
 
     @RequestMapping()
-    public ResponseEntity<String> getPing(@RequestParam("name") String name) {
-        if (Objects.nonNull(name)) {
-            log.info("Was pinged by {}", name);
-        }
+    public ResponseEntity<String> getPing() {
         log.info("Endpoint was pinged");
         return ResponseEntity.ok("Ping");
     }
