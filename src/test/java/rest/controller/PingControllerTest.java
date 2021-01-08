d603c889113c93f8a70b8ec535e198dd758741af
Alexander Sundström <alexander.sundstrom@arbetsformedlingen.se>
2021-01-08 09:38:41 +0100

    Run tests when building docker file

---
+++ src/test/java/rest/controller/PingControllerTest.java
@@ -0,0 +1,22 @@
+package rest.controller;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PingControllerTest {
+
+    private final Logger log = LoggerFactory.getLogger(PingControllerTest.class);
+
+    @Test
+    void getPing() {
+        log.info("Get ping test");
+    }
+
+    @Test
+    void healthchec() {
+        log.info("Healthcheck test");
+    }
+}
